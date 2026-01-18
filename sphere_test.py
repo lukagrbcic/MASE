490462a464d116a5f26425d76119924c1669e24f
lukagrbcic <luka.grbcic@gmail.com>
2026-01-18 07:37:15 -0800

    dynamic tau test

--- sphere_test.py
+++ sphere_test.py
@@ -18,7 +18,7 @@ def rastrigin(x):
         
     return 10 * d + np.sum(x**2 - 10 * np.cos(2 * np.pi * x), axis=-1)
 
-dim=6
+dim=10
 lb = np.ones(dim)*-5.12
 ub = np.ones(dim)*5.12
 
@@ -34,21 +34,25 @@ runs=50
 plt.figure(figsize=(6,5))
 optimizer_lmu = LambdaMU(f, lb, ub, n_evals, mu=n_size)
 x_, fx_ = optimizer_lmu.n_runs(n=runs)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='(Lambda, mu)')
+plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='(Lambda, mu), dyn tau')
 
-optimizer_random = Sampler(f, lb, ub, n_evals, n_size, n_init, seed=seed)
-x_, fx_ = optimizer_random.n_runs(n=runs)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Random')
+optimizer_lmu = LambdaMU(f, lb, ub, n_evals, mu=n_size, tau=0.01)
+x_, fx_ = optimizer_lmu.n_runs(n=runs)
+plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='(Lambda, mu), 0.01 tau')
 
-optimizer_halton = Sampler(f, lb, ub, n_evals, n_size, n_init, method='halton', seed=seed)
-x_, fx_ = optimizer_halton.n_runs(n=50)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Halton')
+# optimizer_random = Sampler(f, lb, ub, n_evals, n_size, n_init, seed=seed)
+# x_, fx_ = optimizer_random.n_runs(n=runs)
+# plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Random')
 
-optimizer_sobol = Sampler(f, lb, ub, n_evals, n_size, n_init, method='sobol', seed=seed)
-x_, fx_ = optimizer_sobol.n_runs(n=50)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Sobol')
+# optimizer_halton = Sampler(f, lb, ub, n_evals, n_size, n_init, method='halton', seed=seed)
+# x_, fx_ = optimizer_halton.n_runs(n=50)
+# plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Halton')
 
-optimizer_lhs = Sampler(f, lb, ub, n_evals, n_size, n_init, method='lhs', seed=seed)
-x_, fx_ = optimizer_lhs.n_runs(n=50)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='LHS')
+# optimizer_sobol = Sampler(f, lb, ub, n_evals, n_size, n_init, method='sobol', seed=seed)
+# x_, fx_ = optimizer_sobol.n_runs(n=50)
+# plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Sobol')
+
+# optimizer_lhs = Sampler(f, lb, ub, n_evals, n_size, n_init, method='lhs', seed=seed)
+# x_, fx_ = optimizer_lhs.n_runs(n=50)
+# plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='LHS')
 plt.legend()
