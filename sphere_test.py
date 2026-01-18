df9516f9ca74604f6d02d8509c8dd701eb19ce0b
lukagrbcic <luka.grbcic@gmail.com>
2026-01-18 07:31:13 -0800

    lambda mu works well on sphere / rastrigin

--- sphere_test.py
+++ sphere_test.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 from sampler import Sampler
+from lambda_mu import LambdaMU
 import matplotlib.pyplot as plt
+import sys
 
 def sphere(x):
     x = np.asarray(x)
@@ -12,35 +14,35 @@ def rastrigin(x):
     
     x = np.asarray(x)
     
-    if len(np.shape(x)) == 1:
-        d = len(x)
-    else:
-        d = np.shape(x)[-1]
-    
-    return 10*d + np.sum(x**2 - 10*np.cos(2*np.pi*x), axis=-1)
+    d = x.shape[-1]
+        
+    return 10 * d + np.sum(x**2 - 10 * np.cos(2 * np.pi * x), axis=-1)
 
-
-dim=2
+dim=6
 lb = np.ones(dim)*-5.12
 ub = np.ones(dim)*5.12
 
-f = rastrigin 
-n_evals = 1024
-n_init = 100
+f = rastrigin
+n_evals = 1000
+n_init = 10
 n_size = 10
 seed=111
-runs=100
+runs=50
+
+
 
 plt.figure(figsize=(6,5))
-optimizer_random = Sampler(f, lb, ub, n_evals, n_size, n_init, seed=seed)
-x_, fx_ = optimizer_random.n_runs(n=50)
-plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Random')
+optimizer_lmu = LambdaMU(f, lb, ub, n_evals, mu=n_size)
+x_, fx_ = optimizer_lmu.n_runs(n=runs)
+plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='(Lambda, mu)')
 
+optimizer_random = Sampler(f, lb, ub, n_evals, n_size, n_init, seed=seed)
+x_, fx_ = optimizer_random.n_runs(n=runs)
+plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Random')
 
 optimizer_halton = Sampler(f, lb, ub, n_evals, n_size, n_init, method='halton', seed=seed)
 x_, fx_ = optimizer_halton.n_runs(n=50)
 plt.plot(np.arange(0, len(fx_[-1]), 1), np.mean(fx_, axis=0), label='Halton')
-
 
 optimizer_sobol = Sampler(f, lb, ub, n_evals, n_size, n_init, method='sobol', seed=seed)
 x_, fx_ = optimizer_sobol.n_runs(n=50)
